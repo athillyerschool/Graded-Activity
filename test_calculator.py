cddf22f8ac333d8d4284bcea781355322f98d40f
Allan Hillyer <hillyeal@oregonstate.edu>
2021-03-11 09:19:48 -0800

    linting errors 2

--- test_calculator.py
+++ test_calculator.py
@@ -3,10 +3,9 @@ import calculator
 
 class TestCalculator:
 
-
     def test_add(self):
-        assert 5 == calculator.add(1,4)
+        assert 5 == calculator.add(1, 4)
     
 
     def test_subtract(self):
-        assert 2 == calculator.subtract(4,2)
+        assert 2 == calculator.subtract(4, 2)
